0b472e697d6294d69cd299a66d2211d5d970223c
Tony Joanes <tony.joanes@mews.com>
2022-09-20 20:19:09 +0100

    add example test

---
+++ src/Marvin.Web.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Marvin.Web.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void WhenRunningTests_ThisTestShouldPass()
+        {
+            Assert.True(true);
+        }
+    }
+}
