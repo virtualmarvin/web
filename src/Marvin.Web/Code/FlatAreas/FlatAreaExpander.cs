a87eaa6c250ebeac189e738ef1d4aec58177e5ac
Tony Joanes <tony.joanes@mews.com>
2022-10-11 21:22:10 +0100

    Add Flat Areas and Routing

---
+++ src/Marvin.Web/Code/FlatAreas/FlatAreaExpander.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Razor;
+
+// Modified from https://github.com/OdeToCode/AddFeatureFolders
+
+namespace Marvin.Web
+{
+    public class FlatAreaExpander : IViewLocationExpander
+    {
+        private readonly string _placeholder;
+
+        public FlatAreaExpander(FlatAreaOptions options)
+        {
+            _placeholder = options.AreaPlaceholder;
+        }
+
+        public void PopulateValues(ViewLocationExpanderContext context)
+        {
+
+        }
+
+        public IEnumerable<string> ExpandViewLocations(
+            ViewLocationExpanderContext context,
+            IEnumerable<string> viewLocations)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            if (viewLocations == null)
+            {
+                throw new ArgumentNullException(nameof(viewLocations));
+            }
+
+            var controllerDescriptor = context.ActionContext.ActionDescriptor as ControllerActionDescriptor;
+            var areaName = controllerDescriptor?.Properties["area"] as string;
+
+            foreach (var location in viewLocations)
+            {
+                yield return location.Replace(_placeholder, areaName);
+            }
+        }
+    }
+}
