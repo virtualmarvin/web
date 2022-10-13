8fcd4378340830f35db391613098114566033899
Tony Joanes <tony.joanes@mews.com>
2022-10-13 14:47:11 +0100

    Fix the privacy page and wireup to homecontroller

---
+++ src/Marvin.Web/Areas/Home/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Marvin.Web.Areas.Home
+{
+    /// <summary>
+    /// The main website controller for Home functions
+    /// </summary>
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Website privacy page
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("Privacy")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
