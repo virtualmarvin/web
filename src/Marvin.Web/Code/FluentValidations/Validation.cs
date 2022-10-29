ac4eabdc774158d8d6aba7218b48e4d8188023a4
Guy Symonds <guy@redspartan.co.uk>
2022-10-29 16:24:13 +0100

    Started Fluent Validation

---
+++ src/Marvin.Web/Code/FluentValidations/Validation.cs
@@ -0,0 +1,33 @@
+﻿namespace Marvin.Web.Code.FluentValidations
+{
+    /// <summary>
+    /// A collection of Validation exceptions
+    /// </summary>
+    public sealed class Validation
+    {
+        private readonly List<Exception> _exceptions = new(1);
+
+        /// <summary>
+        /// A collection of Validation related Exceptions
+        /// </summary>
+        public IEnumerable<Exception> Exceptions => _exceptions;
+
+        public bool Validated { get; } = false;
+
+        /// <summary>
+        /// Add an exception to the collection
+        /// </summary>
+        /// <param name="ex">The exception to add</param>
+        /// <returns>The current Validation</returns>
+        public Validation AddException(Exception ex)
+        {
+            lock (_exceptions)
+            {
+                _exceptions.Add(ex);
+            }
+
+            return this;
+        }
+    }
+
+}
---
+++ src/Marvin.Web/Code/FluentValidations/ValidationExtentions.cs
@@ -0,0 +1,39 @@
+﻿namespace Marvin.Web.Code.FluentValidations
+{
+    /// <summary>
+    /// Extensions for validating conditions
+    /// </summary>
+    public static class ValidationExtensions
+    {
+        /// <summary>
+        /// Check if a parameter is null
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="validation"><see cref="Validation"/></param>
+        /// <param name="theObject">the object to validate</param>
+        /// <param name="paramName">Name of the parameter</param>
+        /// <returns><see cref="Validation"/></returns>
+        public static Validation ArgumentNullCheck<T>(this Validation validation, T theObject, string paramName)
+            where T : class
+        {
+            if (theObject == null)
+                return (validation ?? new Validation()).AddException(new ArgumentNullException(paramName));
+            else
+                return validation;
+        }
+
+        public static Validation? Check(this Validation validation)
+        {
+            if (validation == null)
+                return validation;
+            else
+            {
+                if (validation.Exceptions.Take(2).Count() == 1)
+                    throw new ValidationException(message, validation.Exceptions.First()); // ValidationException is just a standard Exception-derived class with the usual four constructors
+                else
+                    throw new ValidationException(message, new MultiException(validation.Exceptions)); // implementation shown below
+            }
+        }
+    }
+}
+}
